8148d28fba355cf174455d04f4fd8565f44f540d
lizasamuseva <liza.samuseva@gmail.com>
2024-03-04 16:12:28 +0100

    Getter function for attributes height and width, because of protected type of them

--- BoardPiece.py
+++ BoardPiece.py
@@ -1,4 +1,10 @@
 class BoardPiece:
-    def __init__(self, _height, _width):
-        self.height = _height
-        self.width = _width
+    def __init__(self, height, width):
+        self._height = height
+        self._width = width
+
+    def getHeight(self):
+        return self._height
+
+    def getWidth(self):
+        return self._width
