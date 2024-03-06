773722a844b5046c827fc1e4f6cc588c2010225b
lizasamuseva <liza.samuseva@gmail.com>
2024-03-06 19:06:04 +0100

    Added methods isShiftable and Shift to class SideBoardPiece

--- SideBoardPiece.py
+++ SideBoardPiece.py
@@ -1,11 +1,92 @@
 from BoardPiece import BoardPiece
 from Side import Side
+from Vehicle import Vehicle
+from Direction import Direction
 class SideBoardPieces(BoardPiece):
-    def __init__(self, margin, height, width, side):
+    # margin - the number of shifted blocks
+    # +margin  - shift up
+    # -margin - shift down
+    __margin = 0
+    def __init__(self, height, width, side):
         super().__init__(height, width)
-        self.__margin = margin
         self.__side = side
+    def getSide(self):
+        # self.__side = Side.PLUS => side - left
+        # self.__side = Side.MINUS => side - right
+        if self.__side == 0:
+            return Side.PLUS
+        if self.__side == 1:
+            return Side.MINUS
+        if self.__side == Side.PLUS:
+            return Side.PLUS
+        if self.__side == Side.MINUS:
+            return Side.MINUS
     def getMargin(self):
         return self.__margin
-    def getSide(self):
-        return self.__side
+
+    def isShiftable(self, numberToShift, sideToShift, Vehicles):
+        for vehicle in Vehicles:
+            if vehicle.getDirection() == Direction.HORIZONTAL:
+                #Checks for left Board Piece
+                if (self.getSide() == Side.PLUS):
+                    #Checks whether x-coordinates of vehicle crosses the boarder of Board Pieces
+                    if vehicle.getLength() == 2 and abs((vehicle.getPositions()[0][0] - self._width) and (vehicle.getPositions()[1][0] - self._width)) <= 1:
+                        print("False Left Side, 2 length vehicle")
+                        return False
+                    if vehicle.getLength() == 3 and abs(vehicle.getPositions()[2][0] - self._width) <= 1:
+                        print("False Left Side, 3 length vehicle")
+                        return False
+                #Checks for right Board Piece
+                if (self.getSide() == Side.MINUS):
+                    if vehicle.getLength() == 2 and abs((vehicle.getPositions()[0][0] - self._width - 4) and (vehicle.getPositions()[1][0] - self._width - 4)) <= 1:
+                        print("False Right Side, 2 length vehicle")
+                        return False
+                    if vehicle.getLength() == 3 and abs(vehicle.getPositions()[2][0] - self._width - 4) <= 1:
+                        print("False Right Side, 3 length vehicle")
+                        return False
+
+        #Checks whether the shifting is out of boundaries
+        if (sideToShift == Side.PLUS.name or sideToShift == Side.PLUS.value) and (self.__margin + numberToShift < 6):
+            return True
+        if (sideToShift == Side.MINUS.name or sideToShift == Side.MINUS.value) and (self.__margin - numberToShift > -6):
+            return True
+        else:
+            return False
+
+
+
+    def shift (self, numberToShift, sideToShift, Vehicles):
+        if self.isShiftable(numberToShift, sideToShift, Vehicles):
+            #If the Side to Shift Board is UP(PLUS), find the vehicles on it
+            if sideToShift == Side.PLUS.name or sideToShift == Side.PLUS.value:
+                self.__margin += numberToShift
+                for vehicle in Vehicles:
+                    #If (the Board Side is LEFT(PLUS) and vehicle on it) OR (the Board Side is RIGHT(MINUS) and vehicle on it)
+                    #Then change coordinates of vehicle
+                    if(
+                        (self.getSide() == Side.PLUS and (vehicle.getPositions()[0][0] >= 0 and vehicle.getPositions()[0][0] < 5))
+                        or (self.getSide() == Side.MINUS and (vehicle.getPositions()[0][0] >= 9 and vehicle.getPositions()[0][0] < 14))
+                    ):
+                        for coordinates in vehicle.getPositions():
+                            coordinates[1] -= numberToShift
+
+
+
+            # If the Side to Shift Board is DOWN(MINUS), find the vehicles on it
+            if sideToShift == Side.MINUS.name or sideToShift == Side.MINUS.value:
+                self.__margin += -numberToShift
+                # If (the Board Side is LEFT(PLUS) and vehicle on it) OR (the Board Side is RIGHT(MINUS) and vehicle on it)
+                # Then change coordinates of vehicle
+                for vehicle in Vehicles:
+                    if (
+                        (self.getSide() == Side.PLUS and (vehicle.getPositions()[0][0] >= 0 and vehicle.getPositions()[0][0] < 5))
+                        or (self.getSide() == Side.MINUS and (vehicle.getPositions()[0][0] >= 9 and vehicle.getPositions()[0][0] < 14))
+                    ):
+                        for coordinates in vehicle.getPositions():
+                            coordinates[1] += numberToShift
+
+
+
+
+
+
