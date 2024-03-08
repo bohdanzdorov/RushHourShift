6c9f6b7bd81fb625b1a50dc48d2fa300c7509337
lizasamuseva <liza.samuseva@gmail.com>
2024-03-08 16:14:28 +0100

    Added comments and descriptions of restrictions

--- SideBoardPiece.py
+++ SideBoardPiece.py
@@ -13,36 +13,33 @@ class SideBoardPieces(BoardPiece):
     def getSide(self):
         # self.__side = Side.PLUS => side - left
         # self.__side = Side.MINUS => side - right
-        if self.__side == 0:
+        if self.__side == 0 or self.__side == Side.PLUS:
             return Side.PLUS
-        if self.__side == 1:
-            return Side.MINUS
-        if self.__side == Side.PLUS:
-            return Side.PLUS
-        if self.__side == Side.MINUS:
+        if self.__side == 1 or self.__side == Side.MINUS:
             return Side.MINUS
     def getMargin(self):
         return self.__margin
 
     def isShiftable(self, numberToShift, sideToShift, Vehicles):
         for vehicle in Vehicles:
+            #Checks vehicles only in horizontal possition
             if vehicle.getDirection() == Direction.HORIZONTAL:
                 #Checks for left Board Piece
                 if (self.getSide() == Side.PLUS):
                     #Checks whether x-coordinates of vehicle crosses the boarder of Board Pieces
                     if vehicle.getLength() == 2 and abs((vehicle.getPositions()[0][0] - self._width) and (vehicle.getPositions()[1][0] - self._width)) <= 1:
-                        print("False Left Side, 2 length vehicle")
+                        print("It's not impossible to shift this Board, because vehicle is on the boarder.")
                         return False
                     if vehicle.getLength() == 3 and abs(vehicle.getPositions()[2][0] - self._width) <= 1:
-                        print("False Left Side, 3 length vehicle")
+                        print("It's not impossible to shift this Board, because vehicle is on the boarder.")
                         return False
                 #Checks for right Board Piece
                 if (self.getSide() == Side.MINUS):
                     if vehicle.getLength() == 2 and abs((vehicle.getPositions()[0][0] - self._width - 4) and (vehicle.getPositions()[1][0] - self._width - 4)) <= 1:
-                        print("False Right Side, 2 length vehicle")
+                        print("It's not impossible to shift this Board, because vehicle is on the boarder.")
                         return False
                     if vehicle.getLength() == 3 and abs(vehicle.getPositions()[2][0] - self._width - 4) <= 1:
-                        print("False Right Side, 3 length vehicle")
+                        print("It's not impossible to shift this Board, because vehicle is on the boarder.")
                         return False
 
         #Checks whether the shifting is out of boundaries
@@ -51,13 +48,15 @@ class SideBoardPieces(BoardPiece):
         if (sideToShift == Side.MINUS.name or sideToShift == Side.MINUS.value) and (self.__margin - numberToShift > -6):
             return True
         else:
+            print("It's not impossible to shift this Board, because it's already MAXIMUM shifted.")
             return False
 
 
 
     def shift (self, numberToShift, sideToShift, Vehicles):
         if self.isShiftable(numberToShift, sideToShift, Vehicles):
-            #If the Side to Shift Board is UP(PLUS), find the vehicles on it
+            #If the Side to Shift Board is UP(PLUS)
+            #Then find the vehicles on it
             if sideToShift == Side.PLUS.name or sideToShift == Side.PLUS.value:
                 self.__margin += numberToShift
                 for vehicle in Vehicles:
