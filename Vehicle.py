d68817f27344f7e6c91e4e91043a1317ef217954
Bohdan Zdorovets <zdorovetsbogdan@gmail.com>
2024-03-11 21:41:52 +0100

    changes

--- Vehicle.py
+++ Vehicle.py
@@ -1,5 +1,9 @@
+from Side import Side
+from Orientation import Orientation
+import numpy as np
+
 class Vehicle:
-    def __int__(self,id,length,orientation,positions):
+    def __init__(self,id,length,orientation,positions):
         self.__id=id
         self.__length=length
         self.__orientation=orientation
@@ -25,7 +29,7 @@ class Vehicle:
         if empty_blocks>0:
             dx= 0
             dy= 0
-            if self.__orientation == "H":
+            if self.__orientation == Orientation.HORIZONTAL:
                 dx = direction*empty_blocks
             else:
                 dy = direction * empty_blocks
@@ -33,12 +37,12 @@ class Vehicle:
         else:
             return ("Wrong")
 
-    def move(self,direction):
+    def move(self,direction,map):
         empty_blocks = self.getAvailableEmptyBlocks(map, direction)
         if empty_blocks>0:
             dx = 0
             dy = 0
-            if self.__orientation == "H":
+            if self.__orientation == Orientation.HORIZONTAL:
                 dx = direction
             else:
                 dy = direction
@@ -57,23 +61,23 @@ class Vehicle:
         # Therefore, input start_y is the y where the vehicle is located, and start_x + 1 is the first blocks to the right of the vehicle.
         if direction==1:
             start_x,start_y=self.__positions[-1]
-            if self.orientation == "H":
-                empty_blocks = count_positive_consecutive_empty_in_row(map, start_y, start_x + 1)
+            if self.__orientation == Orientation.HORIZONTAL:
+                empty_blocks = self.count_positive_consecutive_empty_in_row(map, start_y, start_x + 1)
 
             else:
-                empty_blocks = count_positive_consecutive_empty_in_row(map.T, start_x, start_y + 1)
+                empty_blocks = self.count_positive_consecutive_empty_in_row(np.map.T, start_x, start_y + 1)
         #         Therefore, if we want to move the "V" car up or down,
         #         then we need to count the number of empty blocks up or down in the column where the vehicle is located.
         #         Therefore, we need to transpose the map first, and then calculate it according to the method function for calculating "H".
 
         else:
-            start_x, start_y = self.position[0]
+            start_x, start_y = self.__positions[0]
             # But if the vehicle's driving direction is "-1", then we need to take the first coordinate of the car's Position,
             # and then calculate the number of empty blocks forward or upward.
-            if self.orientation == "H":
-                empty_blocks = count_negetive_consecutive_empty_in_row(map, start_y, start_x + 1)
+            if self.__orientation == Orientation.HORIZONTAL:
+                empty_blocks = self.count_negetive_consecutive_empty_in_row(map, start_y, start_x + 1)
             else:
-                empty_blocks = count_negetive_consecutive_empty_in_row(map.T, start_x, start_y + 1)
+                empty_blocks = self.count_negetive_consecutive_empty_in_row(np.map.T, start_x, start_y + 1)
 
         return empty_blocks
 
@@ -102,8 +106,10 @@ class Vehicle:
 
         return empty_count
 
+map = [['A', ' '],
+      ['A', ' '],
+      ['_', ' ']]
 
-
-
-
-
+positions = [[0,0], [0,1]]
+v = Vehicle('A', 2, Orientation.VERTICAL, positions)
+v.move(1, map)
